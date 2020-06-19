1979ec7bf313042b34ebf97a967a6d900d5f9943
Irvv17 <irvv.17@gmail.com>
2020-06-19 16:12:36 -0500

    little bit of styling and how to wrap data

--- src/pages/Courses.tsx
+++ src/pages/Courses.tsx
@@ -1,9 +1,29 @@
-import {IonButton, IonContent, IonHeader, IonPage, IonTitle, IonToolbar} from '@ionic/react';
+import {
+    IonButton, IonCard,
+    IonCardContent, IonCol,
+    IonContent,
+    IonGrid,
+    IonHeader,
+    IonPage,
+    IonRow,
+    IonTitle,
+    IonToolbar
+} from '@ionic/react';
 import React from 'react';
 import {useHistory} from "react-router-dom";
 
 import './Home.css';
-
+const DUMMY_DATA = [
+    {
+      id: 'c1', title: 'Ionic = React Course'
+    },
+    {
+        id: 'c2', title: 'Ionic = Angular Course'
+    },
+    {
+        id: 'c3', title: 'Ionic = Vue Course'
+    },
+];
 const Courses: React.FC = () => {
     /**
      * */
@@ -27,14 +47,26 @@ const Courses: React.FC = () => {
           </IonToolbar>
         </IonHeader>
         <IonContent>
-            <div>
-               {/* <IonButton routerLink="/course-goals">
-                    To Course Goals
-                </IonButton>*/}
-                 <IonButton onClick={changePageHandler}>
-                    To Course Goals
-                </IonButton>
-            </div>
+           <IonGrid>
+               {DUMMY_DATA.map(course => {
+                   return (
+                       <IonRow key={course.id}>
+                           <IonCol size-md="4" offset-md="4">
+                               <IonCard>
+                                   <IonCardContent>
+                                       <h2>
+                                           {course.title}
+                                       </h2>
+                                       <IonButton routerLink={`/course/${course.id}`}>
+                                           See Details
+                                       </IonButton>
+                                   </IonCardContent>
+                               </IonCard>
+                           </IonCol>
+                       </IonRow>
+                   )
+               })}
+           </IonGrid>
         </IonContent>
       </IonContent>
     </IonPage>
