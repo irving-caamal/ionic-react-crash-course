3f4ae33f7f24484fd2e67b7b349a321a79db0332
Irvv17 <irvv.17@gmail.com>
2020-06-29 14:49:48 -0500

    showing error messages validating a form using REF react hook

--- src/components/AddCourseModal.tsx
+++ src/components/AddCourseModal.tsx
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {useRef,useState} from 'react';
 import {
     IonButton,
     IonCol,
@@ -7,15 +7,39 @@ import {
     IonHeader,
     IonInput, IonItem, IonLabel,
     IonModal,
-    IonRow,
+    IonRow, IonText,
     IonTitle,
     IonToolbar
 } from "@ionic/react";
+import {save} from "ionicons/icons";
 
 const AddCourseModal : React.FC <{
     show:boolean;
     onCancel: ()=> void;
 }> = props =>{
+    const [error,setError] = useState('');
+    const titleRef = useRef<HTMLIonInputElement>(null);
+    const dateRef = useRef<HTMLIonDatetimeElement>(null);
+
+
+    const saveHandler = () => {
+        const enteredTitle = titleRef.current?.value;
+        const selectedDate = dateRef.current?.value;
+
+        if(
+            !enteredTitle ||
+            !selectedDate ||
+            enteredTitle.toString().trim().length === 0 ||
+            selectedDate.trim().length === 0
+        )
+        {
+            setError('Please enter a valid title and selected a valid date');
+            return;
+        }
+
+        setError('')
+    }
+
     return (
         <IonModal isOpen={props.show}>
             <IonHeader>
@@ -32,7 +56,7 @@ const AddCourseModal : React.FC <{
                       <IonCol>
                           <IonItem>
                               <IonLabel position="floating">Course Title</IonLabel>
-                              <IonInput type="text" />
+                              <IonInput type="text" ref={titleRef} />
                           </IonItem>
                       </IonCol>
                   </IonRow>
@@ -40,16 +64,27 @@ const AddCourseModal : React.FC <{
                       <IonCol>
                           <IonItem>
                               <IonLabel >Enrollement Date</IonLabel>
-                              <IonDatetime displayFormat="MM DD YY" min="2018-06-04"/>
+                              <IonDatetime displayFormat="MM DD YY" min="2018-06-04" ref={dateRef}/>
                           </IonItem>
                       </IonCol>
                   </IonRow>
+                  {error && (
+                      <IonRow className="ion-text-center">
+                          <IonCol>
+                              <IonText color="danger">
+                                  <p>
+                                      {error}
+                                  </p>
+                              </IonText>
+                          </IonCol>
+                      </IonRow>
+                  )}
                   <IonRow >
                       <IonCol>
                          <IonButton color="dark" fill="clear" onClick={props.onCancel}>Cancel</IonButton>
                       </IonCol>
                       <IonCol>
-                          <IonButton color="secondary" expand="block">Save </IonButton>
+                          <IonButton color="secondary" expand="block" onClick={saveHandler}>Save </IonButton>
                       </IonCol>
                   </IonRow>
               </IonGrid>
