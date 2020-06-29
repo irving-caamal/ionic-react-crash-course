d1e0fbf20c9c77202fde6ce063ca3e70e75b76ef
Irvv17 <irvv.17@gmail.com>
2020-06-29 10:44:16 -0500

    Playinf with Items and How React Mouse EVENTS works

--- src/pages/CourseGoals.tsx
+++ src/pages/CourseGoals.tsx
@@ -1,8 +1,8 @@
 import {
-    IonBackButton,
+    IonBackButton, IonButton,
     IonButtons,
     IonContent,
-    IonHeader,
+    IonHeader, IonIcon,
     IonItem, IonLabel,
     IonList,
     IonPage,
@@ -13,6 +13,7 @@ import React from 'react';
 import './Home.css';
 import {useParams} from 'react-router-dom';
 import {DUMMY_DATA} from "./Courses";
+import {create, pencil} from "ionicons/icons";
 
 
 const CourseGoals: React.FC = () => {
@@ -20,7 +21,14 @@ const CourseGoals: React.FC = () => {
     const selectedCourseID = useParams<{id:string}>().id;
     const selectedCourse = DUMMY_DATA.find(course => course.id === selectedCourseID)
 
-
+const deleteItemHandler = (e:React.MouseEvent) => {
+    e.stopPropagation();
+        console.log('deleted')
+}
+    const startEditGoalHandler = (e:React.MouseEvent) => {
+        e.stopPropagation();
+        console.log('edited')
+    }
   return (
     <IonPage>
       <IonHeader>
@@ -36,8 +44,11 @@ const CourseGoals: React.FC = () => {
           <IonList>
               {selectedCourse?.goals.map(goal => {
                   return (
-                      <IonItem key={goal.id} lines="full">
+                      <IonItem key={goal.id} lines="full" button onClick={deleteItemHandler}>
                           <IonLabel>{goal.text}</IonLabel>
+                          <IonButton slot="end" fill="clear" color="dark" onClick={startEditGoalHandler}>
+                              <IonIcon slot="icon-only" icon={create} />
+                          </IonButton>
                       </IonItem>
                   )
               })}
